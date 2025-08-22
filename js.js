af61f628cccef71eda6b0cadc59119ac6fa07f15
Elia <eliasarji900@gmail.com>
2025-08-22 10:55:46 +0300

    added the todo list

--- js.js
+++ js.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
-const port = 300;
+const port = 3000;
 app.use(express.json());
 
-app.get('/',(req,req) =>{
+const todo = ["test1","test2"];
+
+
+
+app.get('/',(req,res) =>{
 
 res.sendFile(__dirname +'/html.html')
-})
+});
+
+app.get('/s',(req,res)=>{
+   res.json(todo)
+});
+
+
 
 app.listen(port,()=>(console.log(`http://localhost:${port}`)));
